6de0d504cbc1c26b5620ddc0a259ee01025a2542
slobodanmiric <slobamiric@gmail.com>
2020-05-26 22:47:25 +0200

    blog webcon

---
+++ blog-webcon/blog.php
@@ -0,0 +1,24 @@
+<?php include "partials/header.php"; ?>
+
+
+    <section class="container rightSidebar">
+        <article>
+            <img src="img/blog1.jpg" alt="">
+            <div class="text">
+                <h2>Blog poste prvi</h2>
+                <hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur saepe alias facilis temporibus obcaecati fugiat, excepturi eveniet nostrum necessitatibus commodi cum quibusdam doloribus dolorem dolor eos molestias quos debitis cupiditate! Fuga, nobis. Voluptatem, voluptatibus, accusantium. Dolores vitae, quo numquam vel quam obcaecati reprehenderit doloribus omnis dolor facilis nostrum, quisquam qui molestiae eligendi inventore non, assumenda quaerat iure tempora error recusandae.
+                </p>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur saepe alias facilis temporibus obcaecati fugiat, excepturi eveniet nostrum necessitatibus commodi cum quibusdam doloribus dolorem dolor eos molestias quos debitis cupiditate! Fuga, nobis. Voluptatem, voluptatibus, accusantium. Dolores vitae, quo numquam vel quam obcaecati reprehenderit doloribus omnis dolor facilis nostrum, quisquam qui molestiae eligendi inventore non, assumenda quaerat iure tempora error recusandae.
+                </p>
+            </div>
+        </article>
+        <?php include "partials/sidebar.php"; ?>
+    </section>
+
+
+
+<?php include "partials/footer.php"; ?>
